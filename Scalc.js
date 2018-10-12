bc1989522dde969b0ecca19968a0404ea356b9ba
Kristober <kriztone1@gmail.com>
2018-10-12 12:50:01 +0000

    3/6

--- Scalc.js
+++ Scalc.js
@@ -3,9 +3,11 @@ function add(numbers) {
 	if(numbers == "")
 	return 0;
 
-	if(numbers.includes(",")) {
+	if(numbers.includes(",","\n")) {
 		var sum = 0;
-		var numArr = numbers.split(",");
+		var numArr = numbers.split(/[,\n;]/);
+
+
 		for(var i = 0; i < numArr.length; i++) {
 			sum += parseInt(numArr[i]);
 		}
